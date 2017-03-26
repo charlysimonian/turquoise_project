2099526b325c03c1480068668e4b8a7d2dac894e
charlysimonian <charly92330@gmail.com>
2017-03-26 21:52:52 +0200

    Add files via upload

--- sample_code/classifier.py
+++ sample_code/classifier.py
@@ -11,8 +11,8 @@ class Classifier(BaseEstimator):
         pass
 
     def fit(self, X, y):
-	estimator = LinearSVC(penalty='l2', loss='squared_hinge', dual=True, tol=0.0001, C=1.0, multi_class='ovr', fit_intercept=True, intercept_scaling=1, class_weight='auto', verbose=0, random_state=None, max_iter=1000)
-	self.clf =  CalibratedClassifierCV(base_estimator=estimator, method='isotonic', cv=5)
+	estimator = RandomForestClassifier(n_estimators=225, criterion='gini', max_depth=140, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_split=1e-07, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
+	self.clf =  CalibratedClassifierCV(base_estimator=estimator, method='sigmoid', cv=5)
 	self.clf.fit(X, y)
 
     def predict(self, X):
